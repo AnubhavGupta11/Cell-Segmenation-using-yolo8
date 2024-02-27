a78e436ec2c4bc42f33fe56251b2c2abfbde5934
Anubhav Gupta <anubhavgupta.2003@gmail.com>
2024-02-27 23:00:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Anubhav Gupta',
+    author_email= 'anubhavgupta.2003@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
